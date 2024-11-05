9b9bb6eb495353f1c6a42324c320634d9328618c
Henry Heino <46334387+personalizedrefrigerator@users.noreply.github.com>
2024-11-05 01:04:44 -0800

    Chore: Disable flaky test: Disable Rich Text Editor case of the `editor.setText` command test (#11311)

--- packages/app-desktop/integration-tests/pluginApi.spec.ts
+++ packages/app-desktop/integration-tests/pluginApi.spec.ts
@@ -3,31 +3,24 @@ import { test } from './util/test';
 import MainScreen from './models/MainScreen';
 
 test.describe('pluginApi', () => {
-	for (const richTextEditor of [false, true]) {
-		test(`the editor.setText command should update the current note (use RTE: ${richTextEditor})`, async ({ startAppWithPlugins }) => {
-			const { app, mainWindow } = await startAppWithPlugins(['resources/test-plugins/execCommand.js']);
-			const mainScreen = new MainScreen(mainWindow);
-			await mainScreen.createNewNote('First note');
-			const editor = mainScreen.noteEditor;
+	test('the editor.setText command should update the current note (use RTE: false)', async ({ startAppWithPlugins }) => {
+		const { app, mainWindow } = await startAppWithPlugins(['resources/test-plugins/execCommand.js']);
+		const mainScreen = new MainScreen(mainWindow);
+		await mainScreen.createNewNote('First note');
+		const editor = mainScreen.noteEditor;
 
-			await editor.focusCodeMirrorEditor();
-			await mainWindow.keyboard.type('This content should be overwritten.');
+		await editor.focusCodeMirrorEditor();
+		await mainWindow.keyboard.type('This content should be overwritten.');
 
-			if (richTextEditor) {
-				await editor.toggleEditorsButton.click();
-				await editor.richTextEditor.click();
-			}
+		await editor.expectToHaveText('This content should be overwritten.');
+		await mainScreen.goToAnything.runCommand(app, 'testUpdateEditorText');
+		await editor.expectToHaveText('PASS');
 
-			await editor.expectToHaveText('This content should be overwritten.');
-			await mainScreen.goToAnything.runCommand(app, 'testUpdateEditorText');
-			await editor.expectToHaveText('PASS');
+		// Should still have the same text after switching notes:
+		await mainScreen.createNewNote('Second note');
+		await editor.goBack();
 
-			// Should still have the same text after switching notes:
-			await mainScreen.createNewNote('Second note');
-			await editor.goBack();
-
-			await editor.expectToHaveText('PASS');
-		});
-	}
+		await editor.expectToHaveText('PASS');
+	});
 });
 
