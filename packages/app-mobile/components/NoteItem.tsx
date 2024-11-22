a7add9961f2f681f0767f12cfdab26f81190c14f
Henry Heino <46334387+personalizedrefrigerator@users.noreply.github.com>
2024-11-22 02:47:22 -0800

    Mobile: Accessibility: Improve note selection screen reader accessibility (#11424)

--- packages/app-mobile/components/NoteItem.tsx
+++ packages/app-mobile/components/NoteItem.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import { PureComponent } from 'react';
+import { memo, useCallback, useMemo } from 'react';
 import { connect } from 'react-redux';
-import { Text, TouchableOpacity, View, StyleSheet, TextStyle, ViewStyle } from 'react-native';
+import { Text, View, StyleSheet, TextStyle, ViewStyle, AccessibilityInfo, TouchableOpacity } from 'react-native';
 import Checkbox from './Checkbox';
 import Note from '@joplin/lib/models/Note';
 import time from '@joplin/lib/time';
@@ -10,6 +10,7 @@ import { _ } from '@joplin/lib/locale';
 import { AppState } from '../utils/types';
 import { Dispatch } from 'redux';
 import { NoteEntity } from '@joplin/lib/services/database/types';
+import useOnLongPressProps from '../utils/hooks/useOnLongPressProps';
 
 interface Props {
 	dispatch: Dispatch;
@@ -19,42 +20,51 @@ interface Props {
 	selectedNoteIds: string[];
 }
 
-interface State {}
 
-type Styles = Record<string, TextStyle|ViewStyle>;
+const useStyles = (themeId: number) => {
+	return useMemo(() => {
+		const theme = themeStyle(themeId);
 
-class NoteItemComponent extends PureComponent<Props, State> {
-	private styles_: Record<string, Styles> = {};
-	public constructor(props: Props) {
-		super(props);
-	}
+		const listItem: ViewStyle = {
+			flexDirection: 'row',
+			// height: 40,
+			borderBottomWidth: 1,
+			borderBottomColor: theme.dividerColor,
+			alignItems: 'flex-start',
+			paddingLeft: theme.marginLeft,
+			paddingRight: theme.marginRight,
+			paddingTop: theme.itemMarginTop,
+			paddingBottom: theme.itemMarginBottom,
+			// backgroundColor: theme.backgroundColor,
+		};
 
-	private styles() {
-		const theme = themeStyle(this.props.themeId);
-		if (this.styles_[this.props.themeId]) return this.styles_[this.props.themeId];
-		this.styles_ = {};
+		const listItemWithCheckbox = { ...listItem };
+		delete listItemWithCheckbox.paddingTop;
+		delete listItemWithCheckbox.paddingBottom;
+		delete listItemWithCheckbox.paddingLeft;
 
-		const styles: Record<string, TextStyle|ViewStyle> = {
-			listItem: {
-				flexDirection: 'row',
-				// height: 40,
-				borderBottomWidth: 1,
-				borderBottomColor: theme.dividerColor,
-				alignItems: 'flex-start',
-				paddingLeft: theme.marginLeft,
-				paddingRight: theme.marginRight,
-				paddingTop: theme.itemMarginTop,
-				paddingBottom: theme.itemMarginBottom,
-				// backgroundColor: theme.backgroundColor,
-			},
-			listItemText: {
-				flex: 1,
-				color: theme.color,
-				fontSize: theme.fontSize,
-			},
-			selectionWrapper: {
-				backgroundColor: theme.backgroundColor,
-			},
+		const listItemText: TextStyle = {
+			flex: 1,
+			color: theme.color,
+			fontSize: theme.fontSize,
+		};
+
+		const listItemTextWithCheckbox = { ...listItemText };
+		listItemTextWithCheckbox.marginTop = theme.itemMarginTop - 1;
+		listItemTextWithCheckbox.marginBottom = listItem.paddingBottom;
+
+		const selectionWrapper: ViewStyle = { };
+
+		const selectionWrapperSelected = { ...selectionWrapper };
+		selectionWrapperSelected.backgroundColor = theme.selectedColor;
+
+		return StyleSheet.create({
+			listItem,
+			listItemText,
+			selectionWrapper,
+			listItemWithCheckbox,
+			listItemTextWithCheckbox,
+			selectionWrapperSelected,
 			checkboxStyle: {
 				color: theme.color,
 				paddingRight: 10,
@@ -66,103 +76,107 @@ class NoteItemComponent extends PureComponent<Props, State> {
 				opacity: 0.4,
 			},
 			uncheckedOpacityStyle: { },
-		};
+		});
+	}, [themeId]);
+};
 
-		styles.listItemWithCheckbox = { ...styles.listItem };
-		delete styles.listItemWithCheckbox.paddingTop;
-		delete styles.listItemWithCheckbox.paddingBottom;
-		delete styles.listItemWithCheckbox.paddingLeft;
+const NoteItemComponent: React.FC<Props> = memo(props => {
+	const styles = useStyles(props.themeId);
 
-		styles.listItemTextWithCheckbox = { ...styles.listItemText };
-		styles.listItemTextWithCheckbox.marginTop = theme.itemMarginTop - 1;
-		styles.listItemTextWithCheckbox.marginBottom = styles.listItem.paddingBottom;
-
-		styles.selectionWrapperSelected = { ...styles.selectionWrapper };
-		styles.selectionWrapperSelected.backgroundColor = theme.selectedColor;
-
-		this.styles_[this.props.themeId] = StyleSheet.create(styles);
-		return this.styles_[this.props.themeId];
-	}
-
-	private todoCheckbox_change = async (checked: boolean) => {
-		if (!this.props.note) return;
+	const todoCheckbox_change = useCallback(async (checked: boolean) => {
+		if (!props.note) return;
 
 		const newNote = {
-			id: this.props.note.id,
+			id: props.note.id,
 			todo_completed: checked ? time.unixMs() : 0,
 		};
 		await Note.save(newNote);
 
-		this.props.dispatch({ type: 'NOTE_SORT' });
-	};
+		props.dispatch({ type: 'NOTE_SORT' });
+	}, [props.note, props.dispatch]);
 
-	private onPress = () => {
-		if (!this.props.note) return;
-		if (this.props.note.encryption_applied) return;
+	const onPress = useCallback(() => {
+		if (!props.note) return;
+		if (props.note.encryption_applied) return;
 
-		if (this.props.noteSelectionEnabled) {
-			this.props.dispatch({
+		if (props.noteSelectionEnabled) {
+			props.dispatch({
 				type: 'NOTE_SELECTION_TOGGLE',
-				id: this.props.note.id,
+				id: props.note.id,
 			});
 		} else {
-			this.props.dispatch({
+			props.dispatch({
 				type: 'NAV_GO',
 				routeName: 'Note',
-				noteId: this.props.note.id,
+				noteId: props.note.id,
 			});
 		}
-	};
+	}, [props.note, props.noteSelectionEnabled, props.dispatch]);
 
-	private onLongPress = () => {
-		if (!this.props.note) return;
+	const onLongPress = useCallback(() => {
+		if (!props.note) return;
 
-		this.props.dispatch({
-			type: this.props.noteSelectionEnabled ? 'NOTE_SELECTION_TOGGLE' : 'NOTE_SELECTION_START',
-			id: this.props.note.id,
+		if (!props.noteSelectionEnabled) {
+			AccessibilityInfo.announceForAccessibility(_('Entering selection mode'));
+		}
+
+		props.dispatch({
+			type: props.noteSelectionEnabled ? 'NOTE_SELECTION_TOGGLE' : 'NOTE_SELECTION_START',
+			id: props.note.id,
 		});
+	}, [props.dispatch, props.note, props.noteSelectionEnabled]);
+
+
+	const note = props.note ?? {};
+	const isTodo = !!Number(note.is_todo);
+	const checkboxChecked = !!Number(note.todo_completed);
+
+	const checkboxStyle = styles.checkboxStyle;
+	const listItemStyle = isTodo ? styles.listItemWithCheckbox : styles.listItem;
+	const listItemTextStyle = isTodo ? styles.listItemTextWithCheckbox : styles.listItemText;
+	const opacityStyle = isTodo && checkboxChecked ? styles.checkedOpacityStyle : styles.uncheckedOpacityStyle;
+	const isSelected = props.noteSelectionEnabled && props.selectedNoteIds.includes(note.id);
+
+	const selectionWrapperStyle = isSelected ? styles.selectionWrapperSelected : styles.selectionWrapper;
+
+	const noteTitle = Note.displayTitle(note);
+
+	const selectDeselectLabel = isSelected ? _('Deselect') : _('Select');
+	const onLongPressProps = useOnLongPressProps({ onLongPress, actionDescription: selectDeselectLabel });
+
+	const contextMenuProps = {
+		// Web only.
+		onContextMenu: onLongPressProps.onContextMenu,
 	};
-
-	public render() {
-		const note = this.props.note ? this.props.note : {};
-		const isTodo = !!Number(note.is_todo);
-		const checkboxChecked = !!Number(note.todo_completed);
-
-		const checkboxStyle = this.styles().checkboxStyle;
-		const listItemStyle = isTodo ? this.styles().listItemWithCheckbox : this.styles().listItem;
-		const listItemTextStyle = isTodo ? this.styles().listItemTextWithCheckbox : this.styles().listItemText;
-		const opacityStyle = isTodo && checkboxChecked ? this.styles().checkedOpacityStyle : this.styles().uncheckedOpacityStyle;
-		const isSelected = this.props.noteSelectionEnabled && this.props.selectedNoteIds.indexOf(note.id) >= 0;
-
-		const selectionWrapperStyle = isSelected ? this.styles().selectionWrapperSelected : this.styles().selectionWrapper;
-
-		const noteTitle = Note.displayTitle(note);
-
-		return (
+	return (
+		<View
+			// context menu listeners need to be added to a parent view of the
+			// TouchableOpacity -- on web, TouchableOpacity registers a custom
+			// onContextMenu handler that can't be overridden.
+			{...contextMenuProps}
+		>
 			<TouchableOpacity
-				onPress={this.onPress}
-				onLongPress={this.onLongPress}
 				activeOpacity={0.5}
-				accessibilityHint={_('Opens note')}
+				onPress={onPress}
 				accessibilityRole='button'
+				accessibilityHint={props.noteSelectionEnabled ? '' : _('Opens note')}
+				aria-pressed={props.noteSelectionEnabled ? isSelected : undefined}
+				accessibilityState={{ selected: isSelected }}
+				{...onLongPressProps}
 			>
-				<View style={selectionWrapperStyle}>
-					<View style={opacityStyle}>
-						<View style={listItemStyle}>
-							{isTodo ? <Checkbox
-								style={checkboxStyle}
-								checked={checkboxChecked}
-								onChange={this.todoCheckbox_change}
-								accessibilityLabel={_('to-do: %s', noteTitle)}
-							/> : null }
-							<Text style={listItemTextStyle}>{noteTitle}</Text>
-						</View>
-					</View>
+				<View style={[selectionWrapperStyle, opacityStyle, listItemStyle]}>
+					{isTodo ? <Checkbox
+						style={checkboxStyle}
+						checked={checkboxChecked}
+						onChange={todoCheckbox_change}
+						accessibilityLabel={_('to-do: %s', noteTitle)}
+					/> : null }
+					<Text style={listItemTextStyle}>{noteTitle}</Text>
 				</View>
 			</TouchableOpacity>
-		);
-	}
-}
+		</View>
+	);
+});
 
 export default connect((state: AppState) => {
 	return {
