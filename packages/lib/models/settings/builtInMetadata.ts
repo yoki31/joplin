955d39bf2e8cd86fc4ae7188c3447d522484f1a3
Laurent Cozic <laurent22@users.noreply.github.com>
2024-12-13 18:28:34 +0100

    iOS: Mark biometric lock feature as stable

--- packages/lib/models/settings/builtInMetadata.ts
+++ packages/lib/models/settings/builtInMetadata.ts
@@ -1585,7 +1585,7 @@ const builtInMetadata = (Setting: typeof SettingType) => {
 		'security.biometricsEnabled': {
 			value: false,
 			type: SettingItemType.Bool,
-			label: () => `${_('Use biometrics to secure access to the app')} (Beta)`,
+			label: () => `${_('Use biometrics to secure access to the app')}${shim.mobilePlatform() !== 'ios' ? ' (Beta)' : ''}`,
 			description: () => 'Important: This is a beta feature and it is not compatible with certain devices. If the app no longer starts after enabling this or is very slow to start, please uninstall and reinstall the app.',
 			show: () => shim.mobilePlatform() !== 'web',
 			public: true,
